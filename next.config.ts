c6a54f50c41d3618369fc17f4f2fafb5cf89c3f3
Robin Keim <uqnbw@student.kit.edu>
2026-03-26 17:25:53 +0100

    GitHub Deployment Configs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const repo = "robin-consulting-site";
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
 };
 
 export default nextConfig;
-
